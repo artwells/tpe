15893120aca845c37ab89369b587799b77e55eca
artwells <artwells@artwells.com>
2024-03-10 17:47:59 +0000

    format and doc

--- lib/low/code_generator.ex
+++ lib/low/code_generator.ex
@@ -26,13 +26,28 @@ defmodule CodeGenerator do
     GenServer.cast(__MODULE__, {:generate_codes_async, count, promo_id})
   end
 
-
   @impl true
   def init(state) do
     {:ok, state}
   end
 
   @impl true
+  @doc """
+  Handles a call to generate codes.
+
+  This function is called when a `:generate_codes` message is received.
+  It fulfills the given count of coupons for the specified promo_id using the max_chunk value.
+
+  ## Params
+
+  - `{:generate_codes, count, promo_id}` - A tuple containing the message and its parameters.
+  - `_from` - The process that sent the message.
+  - `state` - The current state of the process.
+
+  ## Returns
+
+  A tuple `{:reply, :ok, state}` indicating that the call was successful.
+  """
   def handle_call({:generate_codes, count, promo_id}, _from, state) do
     max_chunk = Map.get(state, :max_chunk)
     Low.Coupon.fulfill_count(count, promo_id, max_chunk)
@@ -40,13 +55,32 @@ defmodule CodeGenerator do
   end
 
   @impl true
+  @doc """
+  Generates coupon codes asynchronously.
+
+  This function is called when a `:generate_codes_async` message is cast to the process.
+  It spawns a new process that fulfills the given `count` of coupon codes for the specified `promo_id`.
+  The `max_chunk` value is retrieved from the `state` map.
+
+  ## Params:
+  - `count`: The number of coupon codes to generate.
+  - `promo_id`: The ID of the promotion.
+  - `state`: The current state of the process.
+
+  ## Returns:
+  - `{:noreply, state}`: Indicates that the message has been handled and no reply is expected.
+  """
   def handle_cast({:generate_codes_async, count, promo_id}, state) do
     max_chunk = Map.get(state, :max_chunk)
     spawn(fn -> Low.Coupon.fulfill_count(count, promo_id, max_chunk) end)
     {:noreply, state}
   end
 
-  # handle cast to resert genserver
-
-
+  @doc """
+  Handles a cast to reset the GenServer.
+  """
+  def handle_cast({:reset}, state) do
+    # Reset the state of the GenServer here
+    {:noreply, state}
+  end
 end
--- lib/low/coupon.ex
+++ lib/low/coupon.ex
@@ -1,4 +1,7 @@
 defmodule Low.Coupon do
+  @moduledoc """
+  This module defines the `Low.Coupon` schema and provides functions for creating, updating, and retrieving coupons.
+  """
 
   use Ecto.Schema
   use Ecto.Repo, otp_app: :my_app, adapter: Ecto.Adapters.Postgres
@@ -7,7 +10,19 @@ defmodule Low.Coupon do
 
   @insert_all_timeout 600000
 
+  @doc """
+  The `coupons` schema represents a coupon record in the database.
 
+  ## Fields
+
+  - `code` (`string`): The coupon code.
+  - `active` (`boolean`): Indicates whether the coupon is active or not.
+  - `count` (`integer`): The current count of the coupon.
+  - `max_count` (`integer`): The maximum count allowed for the coupon.
+  - `promo_id` (`integer`): The ID of the associated promo.
+  - `inserted_at` (`utc_datetime`): The timestamp when the coupon was inserted.
+  - `updated_at` (`utc_datetime`): The timestamp when the coupon was last updated.
+  """
   schema "coupons" do
     field :code, :string
     field :active, :boolean
@@ -18,36 +33,89 @@ defmodule Low.Coupon do
     field :updated_at, :utc_datetime
   end
 
-  # Create a changeset
+  @doc """
+  Creates a changeset for a coupon.
 
+  ## Params
+
+  - `coupon` (`Low.Coupon`): The coupon struct.
+  - `params` (`map`): The parameters to update the coupon with.
+
+  ## Returns
+
+  A changeset for the coupon.
+  """
   def changeset(coupon, params \\ %{}) do
     coupon
     |> cast(params, [:code, :active, :count, :max_count, :promo_id, :updated_at])
     |> validate_required([:code, :promo_id])
   end
 
-  # Create a new coupon
+  @doc """
+  Creates a new coupon.
+
+  ## Params
+
+  - `attrs` (`map`): The attributes to create the coupon with.
+
+  ## Returns
+
+  The created coupon.
+  """
   def create_coupon(attrs \\ %{}) do
     %Low.Coupon{}
     |> Low.Coupon.changeset(attrs)
     |> Low.Repo.insert()
   end
 
-  # Increment the count of a coupon
+  @doc """
+  Increments the count of a coupon.
+
+  ## Params
+
+  - `coupon` (`Low.Coupon`): The coupon struct.
+
+  ## Returns
+
+  The updated coupon.
+  """
   def increment_count(coupon) do
     coupon
     |> Ecto.Changeset.change(count: coupon.count + 1)
     |> Low.Repo.update()
   end
 
-  # Change a coupon
+  @doc """
+  Changes a coupon.
+
+  ## Params
+
+  - `coupon` (`Low.Coupon`): The coupon struct.
+  - `attrs` (`map`): The attributes to update the coupon with.
+
+  ## Returns
+
+  The updated coupon.
+  """
   def change_coupon(coupon, attrs \\ %{}) do
-   attrs = Map.put(attrs, :updated_at, DateTime.utc_now())
+    attrs = Map.put(attrs, :updated_at, DateTime.utc_now())
     coupon
     |> Low.Coupon.changeset(attrs)
     |> Low.Repo.update()
   end
 
+  @doc """
+  Retrieves a coupon by ID.
+
+  ## Params
+
+  - `id` (`integer`): The ID of the coupon.
+
+  ## Returns
+
+  - `{:ok, coupon}`: If the coupon is found.
+  - `{:error, :coupon_not_found}`: If the coupon is not found.
+  """
   def get_coupon(id) do
     coupon = Low.Repo.get(Low.Coupon, id)
     cond do
@@ -58,9 +126,20 @@ defmodule Low.Coupon do
     end
   end
 
+  @doc """
+  Retrieves a coupon by code.
+
+  ## Params
+
+  - `code` (`string`): The code of the coupon.
+
+  ## Returns
+
+  - `{:ok, coupon}`: If the coupon is found.
+  - `{:error, :coupon_not_found}`: If the coupon is not found.
+  """
   def get_coupon_by_code(code) do
     coupon = Low.Repo.get_by(Low.Coupon, code: code)
-    #return error if coupon not found
     cond do
       coupon == nil ->
         {:error, :coupon_not_found}
@@ -69,33 +148,70 @@ defmodule Low.Coupon do
     end
   end
 
-  # Get coupon by code checking to see that count is not greater than max_count
+  @doc """
+  Retrieves a valid coupon by code.
+
+  A coupon is considered valid if it is active and the count is not greater than the max_count.
+
+  ## Params
+
+  - `code` (`string`): The code of the coupon.
+
+  ## Returns
+
+  - `{:ok, coupon}`: If the coupon is valid.
+  - `{:error, :coupon_not_active}`: If the coupon is not active.
+  - `{:error, :coupon_count_greater_than_max_count}`: If the coupon count is greater than the max_count.
+  - `{:error, :coupon_not_found}`: If the coupon is not found.
+  """
   def get_valid_coupon(code) do
-    case get_coupon_by_code(code)
-    do
+    case get_coupon_by_code(code) do
       {:ok, coupon} ->
         cond do
-        coupon.active != :true ->
-          {:error, :coupon_not_active}
-        coupon.count >= coupon.max_count ->
-          {:error, :coupon_count_greater_than_max_count}
-        true ->
-          {:ok, coupon}
-      end
+          coupon.active != :true ->
+            {:error, :coupon_not_active}
+          coupon.count >= coupon.max_count ->
+            {:error, :coupon_count_greater_than_max_count}
+          true ->
+            {:ok, coupon}
+        end
       {:error, error} ->
         {:error, error}
     end
   end
 
-  # insert a map of coupons
+  @doc """
+  Inserts a map of coupons into the database.
 
+  ## Params
+
+  - `coupons` (`list`): The list of coupons to insert.
+
+  ## Returns
+
+  The number of successfully inserted coupons.
+  """
   def insert_coupons(coupons) do
     Low.Repo.transaction(fn ->
       Low.Repo.insert_all(Low.Coupon, coupons, on_conflict: :nothing)
-
     end, timeout: @insert_all_timeout) # Set the timeout value in milliseconds
-
   end
+
+  @doc """
+  Fulfills the count of coupons for a promo.
+
+  This function generates random codes and inserts them into the database until the desired count is reached.
+
+  ## Params
+
+  - `count` (`integer`): The desired count of coupons.
+  - `promo_id` (`integer`): The ID of the associated promo.
+  - `max_chunk` (`integer`): The maximum number of codes to insert in a single transaction.
+
+  ## Returns
+
+  - `{:ok, success_count}`: If the count is fulfilled successfully.
+  """
   def fulfill_count(count, promo_id, max_chunk \\ 10000) do
     codes = generate_random_codes(count, promo_id)
     success_count = Enum.reduce(chunk(codes, max_chunk), 0, fn chunk, acc ->
@@ -107,18 +223,34 @@ defmodule Low.Coupon do
       fulfill_count(count - success_count, promo_id, max_chunk)
     end
     {:ok, success_count}
-
   end
 
-
+  # Generates random codes for a given count and promo_id.
+  #
+  # Args:
+  #   - count: The number of codes to generate.
+  #   - promo_id: The ID of the promo associated with the codes.
+  #
+  # Returns:
+  #   A list of maps, each containing a randomly generated code and the promo_id.
   defp generate_random_codes(count, promo_id) do
     Enum.map(1..count, fn _ ->
       %{code: :crypto.strong_rand_bytes(3) |> Base.encode64(), promo_id: promo_id}
     end)
   end
 
-
-
+  # Chunks a list into sublists of a specified size.
+  #
+  # Examples
+  #
+  #   iex> chunk([1, 2, 3, 4, 5, 6], 2)
+  #   [[1, 2], [3, 4], [5, 6]]
+  #
+  #   iex> chunk([1, 2, 3, 4, 5, 6], 3)
+  #   [[1, 2, 3], [4, 5, 6]]
+  #
+  #   iex> chunk([1, 2, 3, 4, 5, 6], 4)
+  #   [[1, 2, 3, 4], [5, 6]]
   defp chunk(list, size) when is_list(list) and is_integer(size) and size > 0 do
     Enum.chunk_every(list, size)
   end
