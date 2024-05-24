48431d068ac8e0ecc2cccbf66086ec41ba875020
artwells <artwells@artwells.com>
2024-05-24 17:49:13 +0000

    read comments standardized

--- lib/tpe/rule_part/read.ex
+++ lib/tpe/rule_part/read.ex
@@ -1,8 +1,5 @@
 
 defmodule Tpe.RulePart.Read do
-#@compile ({:nowarn_unused_function, {:process_arguments, 1}})
-# @compile {:nowarn_unused_function, {:gather_rule_parts, 2}}
-# not sure what to do about this
 
 @moduledoc """
   This module provides functions for reading rule parts from the database.
@@ -15,20 +12,29 @@ defmodule Tpe.RulePart.Read do
   @doc """
   Retrieves a rule part by its ID.
 
+  ## Params:
+    - id: The ID of the rule part to retrieve.
+
+  ## Returns:
+    A tuple containing the result of retrieving the rule part:
+    - `:ok` - If the rule part was successfully retrieved.
+    - `rule_part` - The retrieved rule part.
+    - `:error` - If the rule part was not found.
+    - `:rule_part_not_found` - If the rule part was not found.
+
   ## Examples
-  iex> {:ok, rule} = Tpe.Rule.Create.create_rule(%{name: "Rule 1", description: "a new rule"})
-  iex> attrs = %{rule_id: rule.id, block: "tester block", verb: "verb", arguments: %{}}
-  iex> {:ok, rule_part1} = Create.create_rule_part(attrs)
-  iex> {:ok, rule_part1} = Tpe.RulePart.Read.get_rule_part(rule_part1.id)
-  iex> rule_part1.rule_id
-  rule.id
+    iex> {:ok, rule} = Tpe.Rule.Create.create_rule(%{name: "Rule 1", description: "a new rule"})
+    iex> attrs = %{rule_id: rule.id, block: "tester block", verb: "verb", arguments: %{}}
+    iex> {:ok, rule_part1} = Create.create_rule_part(attrs)
+    iex> {:ok, rule_part1} = Tpe.RulePart.Read.get_rule_part(rule_part1.id)
+    iex> rule_part1.rule_id
+    rule.id
   """
   def get_rule_part(id) do
     rule_part = Tpe.Repo.get(Tpe.RulePart, id)
     cond do
       rule_part == nil ->
         {:error, :rule_part_not_found}
-
       true ->
         {:ok, rule_part}
     end
@@ -37,15 +43,27 @@ defmodule Tpe.RulePart.Read do
   @doc """
   Retrieves a list of rule parts by the rule ID.
 
+  ## Params:
+    - rule_id: The ID of the rule to retrieve rule parts for.
+    - verb: The verb to filter the rule parts by. If not provided, all rule parts for the rule are retrieved.
+
+  ## Returns:
+    A tuple containing the result of retrieving the rule parts:
+    - `:ok` - If the rule parts were successfully retrieved.
+    - `rule_parts` - The retrieved rule parts.
+    - `:error` - If the rule parts were not found.
+    - `:rule_part_not_found` - If the rule parts were not found.
+
   ## Examples
-  iex> {:ok, rule} = Tpe.Rule.Create.create_rule(%{name: "Rule 1", description: "a new rule"})
-  iex> attrs = %{rule_id: rule.id, block: "tester block", verb: "verb", arguments: %{}}
-  iex> {:ok, _} = Create.create_rule_part(attrs)
-  iex> attrs = %{rule_id: rule.id, block: "tester block", verb: "verb", arguments: %{}}
-  iex> {:ok, _} = Create.create_rule_part(attrs)
-  iex> {:ok, [rule_part_1, _]} = Tpe.RulePart.Read.list_rule_parts_by_rule_id(rule.id)
-  iex> rule_part_1.rule_id
-  rule.id
+    iex> {:ok, rule} = Tpe.Rule.Create.create_rule(%{name: "Rule 1", description: "a new rule"})
+    iex> attrs = %{rule_id: rule.id, block: "tester block", verb: "verb", arguments: %{}}
+    iex> {:ok, _} = Create.create_rule_part(attrs)
+    iex> attrs = %{rule_id: rule.id, block: "tester block", verb: "verb", arguments: %{}}
+    iex> {:ok, _} = Create.create_rule_part(attrs)
+    iex> {:ok, [rule_part_1, _]} = Tpe.RulePart.Read.list_rule_parts_by_rule_id(rule.id)
+    iex> rule_part_1.rule_id
+    rule.id
+
   """
   def list_rule_parts_by_rule_id(rule_id, verb \\ nil) do
     rule_parts = case verb do
@@ -57,36 +75,28 @@ defmodule Tpe.RulePart.Read do
     cond do
       rule_parts in [nil, []] ->
         {:error, :rule_part_not_found}
-
       true ->
         {:ok, rule_parts}
     end
   end
 
 
-
-  # Sorts and gathers rule parts based on specific criteria.
-    #
-    # This function sorts and gathers rule parts based on the order and the type of rule part.
+  # Sorts and gathers the given rule parts based on their order and verb.
     #
     # Params:
     # - rule_parts: A list of rule parts to be sorted and gathered.
-    #
-    # Returns:
-    # A sorted list of rule parts.
+    #  Returns:
+    # A list of maps where the keys are the blocks and the values are lists of statements
+
+
     defp sort_and_gather(rule_parts) do
       rule_parts
-      |> Enum.sort_by(&Map.get(&1.arguments, "order"))
-        |> Enum.sort_by(fn
-        %{verb: "has"} -> 0
-        %{verb: "assign"} -> 1
-        %{block: "do"} -> 2
-      end)
+      |> Enum.sort_by(&{&1.order, &1.verb})
     end
 
   # Processes the arguments and returns a map with atom keys and corresponding values.
   #
-  # Arguments:
+  # Params:
   #   - arguments: a list of key-value pairs
   #
   # Returns:
@@ -116,7 +126,12 @@ defmodule Tpe.RulePart.Read do
   #
   # Raises:
   #   - "Unknown verb" if the verb in the rule_part is not recognized.
-
+  #
+  # Examples:
+  #   iex> rule_part = %{verb: "has", arguments: %{subject: "Alice", predicate: "likes", object: "Bob"}}
+  #   iex> get_statement(rule_part)
+  #   iex> has("Alice", "likes", "Bob")
+  #
   defp get_statement(rule_part) do
     import Wongi.Engine.DSL
     arguments = Map.get(rule_part, :arguments)
@@ -174,27 +189,27 @@ defmodule Tpe.RulePart.Read do
     end)
   end
 
-@doc """
-  Retrieves and processes rule parts for a given rule ID.
-
-  ## Parameters
-  - `rule_id` - The ID of the rule to retrieve and process rule parts for.
-
-  ## Returns
-  A tuple containing the result of retrieving and processing the rule parts:
-  - `:ok` - If the rule parts were successfully retrieved and processed.
-  - `rule_parts` - The processed rule parts.
-
-  ## Examples
-
-      get_processed_rule_parts(123)
-
-      # Output:
-      # {:ok, [processed_rule_part1, processed_rule_part2, ...]}
-  """
+  # Retrieves and processes rule parts for a given rule ID.
+  #
+  # ## Parameters
+  # - `rule_id` - The ID of the rule to retrieve and process rule parts for.
+  #
+  # ## Returns
+  # A tuple containing the result of retrieving and processing the rule parts:
+  # - `:ok` - If the rule parts were successfully retrieved and processed.
+  # - `rule_parts` - The processed rule parts.
+  #
+  # ## Examples
+  #
+  #     get_processed_rule_parts(123)
+  #
+  #     # Output:
+  #     # {:ok, [processed_rule_part1, processed_rule_part2, ...]}
+  #
   def get_processed_rule_parts(rule_id) do
     {:ok, rule_parts} = list_rule_parts_by_rule_id(rule_id)
-    rule_parts |> sort_and_gather()
+    #rule_parts |> sort_rule_parts() |> sort_assigns|> gather_rule_parts()
+    rule_parts |> sort_and_gather() |> gather_rule_parts()
   end
 
 end
